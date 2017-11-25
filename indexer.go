a65d356671ebced95e9dcda09a1f469dd7baa807
Aaron Bloom <contact@aaronbloom.uk>
2017-11-25 13:23:58 +0000

    Fixed references. Included skip logic (node_modules).

--- indexer.go
+++ indexer.go
@@ -1,21 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"os"
 	"path/filepath"
 	"strings"
-	"os"
+	"time"
 )
 
-func storeRecord(path string, records []recordItem, termIndex []indexItem) {
-	currentRecord := recordItem{
-		id: len(records),
-		content: path,
-	}
-	records = append(records, currentRecord)
+func storeRecord(path string, records *[]recordItem, termIndex *[]indexItem) {
+	currentRecord := recordItem{len(*records), path}
+	*records = append(*records, currentRecord)
 
-	terms := strings.FieldsFunc(path, pathSplit);
+	terms := strings.FieldsFunc(path, pathSplit)
 	for i := 0; i < len(terms); i++ {
 		addRecordToIndex(terms[i], currentRecord.id, termIndex)
 	}
@@ -25,11 +23,11 @@ func pathSplit(r rune) bool {
 	return r == '/' || r == '\\' || r == ' ' || r == '.'
 }
 
-func addRecordToIndex(term string, recordID int, termIndex []indexItem) {
+func addRecordToIndex(term string, recordID int, termIndex *[]indexItem) {
 	term = neutralString(term)
-	for j := 0; j < len(termIndex); j++ {
-		if (termIndex[j].term == term) {
-			addRecordToTerm(&termIndex[j], recordID)
+	for j := 0; j < len(*termIndex); j++ {
+		if (*termIndex)[j].term == term {
+			addRecordToTerm(termIndex, recordID, j)
 			return
 		}
 	}
@@ -43,17 +41,17 @@ func neutralString(s string) string {
 	return s
 }
 
-func addRecordToTerm(term *indexItem, recordID int) {
-	term.records = append(term.records, recordID)
+func addRecordToTerm(termIndex *[]indexItem, recordID int, index int) {
+	(*termIndex)[index].records = append((*termIndex)[index].records, recordID)
 }
 
-func insertNewTerm(term string, recordID int, termIndex []indexItem) {
+func insertNewTerm(term string, recordID int, termIndex *[]indexItem) {
 	index := indexItem{
-		id: len(termIndex),
-		term: term,
+		id:      len(*termIndex),
+		term:    term,
 		records: []int{recordID},
 	}
-	termIndex = append(termIndex, index)
+	*termIndex = append(*termIndex, index)
 }
 
 func indexPath(startPath string) ([]recordItem, []indexItem) {
@@ -65,21 +63,26 @@ func indexPath(startPath string) ([]recordItem, []indexItem) {
 
 	startTime := time.Now()
 
-	filepath.Walk(startPath, func (path string, f os.FileInfo, err error) error {
+	filepath.Walk(startPath, func(path string, f os.FileInfo, err error) error {
 		totalCount++
+
+		if strings.Contains(path, "node_modules") {
+			fmt.Println("Skipping directory", path)
+			return errors.New("Skipping directory")
+		}
 		if strings.HasSuffix(path, ".exe") {
 			matchCount++
-			storeRecord(path, records, termIndex)
+			storeRecord(path, &records, &termIndex)
 			fmt.Printf("(%d) Visited: %s\n", matchCount, path)
 		}
-	
+
 		return nil
 	})
 
 	timeTaken := time.Since(startTime)
 
 	fmt.Printf("Indexing time taken: %s\n", timeTaken.String())
-	fmt.Printf("%fs per item\n", timeTaken.Seconds() / float64(totalCount))
+	fmt.Printf("%fs per item\n", timeTaken.Seconds()/float64(totalCount))
 
 	fmt.Printf("Total items found: %d\n", totalCount)
 	fmt.Printf("Total exes found: %d\n", matchCount)
